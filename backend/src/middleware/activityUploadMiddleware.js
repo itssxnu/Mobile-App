f1f443a0b03c78ba59b88a63d36df423e7951349
LONETH <IT24100938@my.sliit.lk>
2026-05-01 16:33:34 +0530

    activity upload middleware finish

--- backend/src/middleware/activityUploadMiddleware.js
+++ backend/src/middleware/activityUploadMiddleware.js
@@ -19,4 +19,23 @@ const storage = multer.diskStorage({
     },
 });
 
+function checkFileType(file, cb) {
+    const filetypes = /jpg|jpeg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
 
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb('Images only!');
+    }
+}
+
+const uploadActivityImage = multer({
+    storage,
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    },
+});
+
+module.exports = uploadActivityImage;
